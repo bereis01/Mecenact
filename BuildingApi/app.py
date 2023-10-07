32a31c82ded848ee681a73cc2ccda912f4264a5b
MatheusFarnese <95251433+MatheusFarnese@users.noreply.github.com>
2023-10-07 17:29:21 -0300

    Image related methods, get all posts, get all tags, get an specific post

--- BuildingApi/app.py
+++ BuildingApi/app.py
@@ -20,6 +20,11 @@ def get_user(name, password):
         return get_posts_user(name)
     return "Wrong password\n", 403
 
+@app.get("/posts")
+def all_posts():
+    result = db.all_posts()
+    return jsonify(result), 200
+
 @app.get("/posts/user/<name>")
 def get_posts_user(name):
     result = db.posts_from_user(name)
@@ -43,9 +48,24 @@ def get_posts_tags_plus(tags_t, tags_f):
             result.append(element)
     return jsonify(result), 200 
 
+@app.get("/posts/<author>/<post_id>")
+def get_post(author, post_id):
+    result = db.return_post(author, post_id)
+    return jsonify(result), 200 
+
 @app.get("/tags/<author>/<post_id>")
 def get_tags(author, post_id):
     result = db.tags_from_post(author, post_id)
+    return jsonify(result), 200 
+
+@app.get("/tags")
+def all_tags():
+    result = db.all_tags()
+    return jsonify(result), 200
+
+@app.get("/image/<author>/<post_id>")
+def get_image(author, post_id):
+    result = db.image_from_post(author, post_id)
     return jsonify(result), 200 
 
 @app.post("/user")
@@ -60,7 +80,7 @@ def add_user():
 def add_posts():
     if request.is_json:
         post = request.get_json()
-        (author, post_id) = db.create_post(post[0], post[1], post[2], post[3])
+        (author, post_id) = db.create_post(post[0], post[1], post[2])
         result = db.return_post(author, post_id)
         return jsonify(result), 201 
     return {"error": "Request must be JSON"}, 415 
@@ -72,6 +92,14 @@ def update_tags(author, post_id):
         tag_array = info[0].split(",")
         db.assign_tags(tag_array, author, post_id)
         return jsonify(tag_array), 200 
+    return {"error": "Request must be JSON"}, 415 
+
+@app.put("/image/<author>/<post_id>")
+def update_image(author, post_id):
+    if request.is_json:
+        info = request.get_json()
+        db.assign_image(info[0], info[1], author, post_id)
+        return jsonify(info[0]), 200 
     return {"error": "Request must be JSON"}, 415 
 
 @app.put("/user/<name>/<password>")
@@ -103,4 +131,9 @@ def delete_user(name, password):
 def delete_tags(tags, author, post_id):
     tag_array = tags.split(",")
     result = db.remove_tags(tag_array, author, post_id)
+    return "Delete successful\n", 204
+
+@app.delete("/image/<author>/<post_id>")
+def delete_image(tags, author, post_id):
+    result = db.remove_image(author, post_id)
     return "Delete successful\n", 204
